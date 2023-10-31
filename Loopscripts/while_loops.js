fa9415487b79ce46576fbfb33332aefeef7873cd
jaquellsensi <146892715+jaquellsensi@users.noreply.github.com>
2023-10-31 14:01:39 -0700

    loops

---
+++ Loopscripts/while_loops.js
@@ -0,0 +1,14 @@
+for(let i = 0; i < 7; i++) {
+ 
+    console.log("I love loops");
+}
+
+console.log("--------------------------");
+
+let counter = 0;
+
+while(counter < 7) {
+    console.log("I love loops");
+
+    counter++;
+}
